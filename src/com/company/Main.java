11b952e557e2ea6ab1bc63d07ea6697c47887a36
Ronan Cassidy <ronanmcassidy@gmail.com>
2021-06-22 13:29:20 +0100

    Completion of 04 05 Exercise utilising the various numeric to string formatters in Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
         var doubleValue = 10_000_000.53;
+
+        //In this exercise we are formatting numeric values as strings
+
+        var numF = NumberFormat.getNumberInstance();
+        System.out.println("Number:" + numF.format(doubleValue));
+
+        var intF = NumberFormat.getIntegerInstance();
+        System.out.println("Number:" + intF.format(doubleValue));
+
+        intF.setGroupingUsed(false);
+        System.out.println("Number:" + intF.format(doubleValue));
+
+        //Allows us to specify a locale, very useful for setting to local standards
+        var locale = new Locale("de","DE");
+        var localFormatter = NumberFormat.getNumberInstance(locale);
+        System.out.println("Number:" + localFormatter.format(doubleValue));
+
+        //Currency formatting
+        var currencyFormatter = NumberFormat.getCurrencyInstance(locale);
+        System.out.println(currencyFormatter.format(doubleValue));
+
+        //Printing value using a pattern
+        var df = new DecimalFormat("€00.00");
+        System.out.println(df.format(doubleValue));
     }
 }
