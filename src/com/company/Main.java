662ce04febe5a9e70a9b6363d99a8a8a11160df7
Ronan Cassidy <ronanmcassidy@gmail.com>
2021-06-22 13:34:49 +0100

    Completion of 04 06 Exercise utilising String.format with a template to print out the data we want to display in a particular fashion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,14 @@ public class Main {
         var price = 14.99;
         var color = "Red";
 
+        // We can utilise %s as a holder for a string value, %f for a float
+        //Adding .2 in the middle of %f truncates it to 2 decimal places
+        var template = "Clothing iem: %s, size %s, color %s, $%.2f";
+
+        //Then we can pass this template to String.Format and as many objects as we need
+        var itemString = String.format(template,item,size,color,price);
+
+        System.out.println(itemString);
+
     }
 }
