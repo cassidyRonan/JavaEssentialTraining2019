55e35d7df06edbf309390bbe3258ffe9274db7ce
Ronan Cassidy <ronanmcassidy@gmail.com>
2021-06-22 13:42:13 +0100

    Completion of 04 07 Exercise
    Exercise showing how string comparison works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,42 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        //This will work because java is checking memory for a value that matches S2 and then points to it hence the objects are the same
         String s1 = "Hello!";
         var s2 = "Hello!";
+
+        if(s1 == s2){
+            System.out.println("They match!");
+        }
+        else{
+            System.out.println("They don't match!");
+        }
+
+        //This won't work because s3 and s4 are two seperate objects
+        String s3 = new String("Hello!");
+        String s4 = new String("Hello!");
+        if(s3 == s4){
+            System.out.println("They match!");
+        }
+        else{
+            System.out.println("They don't match!");
+        }
+
+        //Use the built in string method equals to avoid this issue
+        if(s3.equals(s4)){
+            System.out.println("They match!");
+        }
+        else{
+            System.out.println("They don't match!");
+        }
+
+        //We can also ignore case using an alternate method
+        if(s3.equalsIgnoreCase(s4)){
+            System.out.println("They match!");
+        }
+        else{
+            System.out.println("They don't match!");
+        }
     }
 }
