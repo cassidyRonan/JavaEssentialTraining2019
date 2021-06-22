50eb495185b59f351c36b88c8058bb4a05ab652d
Ronan Cassidy <ronanmcassidy@gmail.com>
2021-06-22 13:17:36 +0100

    Completion of 04 04 Exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,18 @@ public class Main {
         int result = 10 + 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        var result2 = 10 + 20;
+
+        int intVal = 42;
+        var fromInt = Integer.toString(intVal);
+        System.out.println(fromInt);
+
+        boolean boolValue = true;
+        var fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
     }
 }
